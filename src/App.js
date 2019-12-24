cb974a6544782d79f1a36e0bcf09d87140c0d47c
Spencer Lyons <sjl8r8@gmail.com>
2019-12-24 12:40:49 -0600

    added Footer component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from './components/Home';
 import AboutMe from './components/AboutMe'
 import Skills from './components/Skills';
 import Projects from './components/projects/Projects';
-import Contact from './components/Contact'
+import Contact from './components/Contact';
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return <footer id="footer">
+    <p>&copy; Spencer Lyons</p>
+    <p>Established In: 1986</p>
+</footer>
+}
+
+export default Footer;
