c78c30dcc1149cfcba1169ab1079e030f58325e4
Spencer Lyons <sjl8r8@gmail.com>
2019-12-27 16:35:08 -0600

    got hover working using mouseOver and Out funcs.. need to get a new build and test backend

--- frontend/src/components/projects/ProjectCard.jsx
+++ frontend/src/components/projects/ProjectCard.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
+
 
 
 function ProjectCard(props) {
+  const [hover, setHover] = useState(false);
+
+function handleOver() {
+  setHover(true)
+}
+
+function handleOut() {
+  setHover(false)
+}
   return (
-    <div className="card bg-dark text-white">
-      <img className="card-img" src={props.img} alt="my projects" />
-      <div className="card-img-overlay">
+    <div className="card bg-dark text-white" onMouseOver={handleOver} onMouseOut={handleOut} >
+      <img className="card-img" src={props.img} alt="my projects"/>
+      <div className="card-img-overlay" style={hover ? {display: 'block'} : {display: 'none'}}>
         <a href={props.link} target="_blank" rel="noopener noreferrer">
           <h5 className="card-title">{props.title}</h5>
         </a>
