abb781bf6e29166dcec54c4d2c8f497c96d3eba4
Spencer Lyons <sjl8r8@gmail.com>
2019-12-27 17:35:31 -0600

    seeing if changing gitignore fixed heroku error

---
+++ frontend/build/precache-manifest.f7f2f13531a1731faa9d27bc86ec6b94.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591a7e53f91e584dd8ae5e1318f2a032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b10ee4fbbf1bc50bf71",
+    "url": "/static/css/main.0e5aea8e.chunk.css"
+  },
+  {
+    "revision": "7940b8717d760f81d267",
+    "url": "/static/js/2.274b8263.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.274b8263.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b10ee4fbbf1bc50bf71",
+    "url": "/static/js/main.3408d268.chunk.js"
+  },
+  {
+    "revision": "b6d641d7babc028ccdf2",
+    "url": "/static/js/runtime-main.712fe5d1.js"
+  },
+  {
+    "revision": "56c47762eedeae6935e6e249f044d21e",
+    "url": "/static/media/8ball.56c47762.PNG"
+  },
+  {
+    "revision": "ebe676afd24097341239528d925ce814",
+    "url": "/static/media/bitcoin.ebe676af.PNG"
+  },
+  {
+    "revision": "3339b1857f2f199abef2edfd534aae45",
+    "url": "/static/media/blackjack.3339b185.PNG"
+  },
+  {
+    "revision": "703679b8d41ea8e552e80d814730aa5d",
+    "url": "/static/media/blog.703679b8.PNG"
+  },
+  {
+    "revision": "05c02ccfd14874cbc1c95919700cd87b",
+    "url": "/static/media/bowl.05c02ccf.PNG"
+  },
+  {
+    "revision": "7992498a72dbe3c58594a691410e1861",
+    "url": "/static/media/budget.7992498a.PNG"
+  },
+  {
+    "revision": "5a33ca8c98d5a3f2dab47ae5a35e0eb8",
+    "url": "/static/media/dice.5a33ca8c.PNG"
+  },
+  {
+    "revision": "9f3db630dc92f1db0af285922074743d",
+    "url": "/static/media/drum.9f3db630.PNG"
+  },
+  {
+    "revision": "1f61ed9c67ed654ac1776e16e8445cd3",
+    "url": "/static/media/me2.1f61ed9c.png"
+  },
+  {
+    "revision": "698977d7407b89bb634f826c9f9531b1",
+    "url": "/static/media/night2.698977d7.jpg"
+  },
+  {
+    "revision": "cab0c0c3a7bd3956c77d1bc010d1d215",
+    "url": "/static/media/simon.cab0c0c3.PNG"
+  },
+  {
+    "revision": "bb389702ccc48a7d498da682aa9c0f4f",
+    "url": "/static/media/socialup.bb389702.PNG"
+  },
+  {
+    "revision": "f0c1de2adc0bbcdb41dc38f7889bea4d",
+    "url": "/static/media/tindog.f0c1de2a.PNG"
+  },
+  {
+    "revision": "6b2e1157c5cff61420bc096f6377de93",
+    "url": "/static/media/todo.6b2e1157.PNG"
+  },
+  {
+    "revision": "7d179d5d903b47fcc72c87230f93253f",
+    "url": "/static/media/waterfall.7d179d5d.jpg"
+  }
+]);
